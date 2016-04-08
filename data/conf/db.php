6f71acce6277b34246b2ba52b12e5ba0987cdda2
Jake <singviy@qq.com>
2016-04-08 17:18:53 +0800

    add thinkcmf files

---
+++ data/conf/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'myblog',
+    'DB_USER' => 'root',
+    'DB_PWD' => '',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'mb_',
+    //密钥
+    "AUTHCODE" => 'znYyMmvg7ePnIZYFc2',
+    //cookies
+    "COOKIE_PREFIX" => 'gN72Lk_',
+);
