29f10b0cedf61e62c39893a873ae85085e18bd2b
Jake <singviy@qq.com>
2016-04-10 14:36:38 +0800

    。。。

--- application/Home/Controller/ListController.class.php
+++ application/Home/Controller/ListController.class.php
@@ -8,6 +8,8 @@
 // +----------------------------------------------------------------------
 namespace Home\Controller;
 use Common\Controller\HomebaseController;
+use Think\Page;
+
 /**
  * 文章列表
 */
@@ -41,7 +43,24 @@ class ListController extends HomebaseController {
         {
             $post_ids = ['0'];
         }
-		$posts = mb_get_post_list(array('id'=>array('in',$post_ids)));
+        
+        $count = M('Posts')->where(['post_status'=>1,'id'=>array('in',$post_ids)])->count();
+        $Page = new Page($count,5);
+        $pages = $Page->show();
+        $posts = M('Posts')->where(['post_status'=>1,'id'=>array('in',$post_ids)])->order("post_date desc")
+            ->limit($Page->firstRow.','.$Page->listRows)
+            ->select();
+
+        if($posts){
+            foreach ($posts as $key => $value){
+                $posts[$key]['author_name'] = M('Users')->where(['id'=>$value['post_author']])->getField('user_nicename');
+                $posts[$key]['show_date']   = date('Y年m月d日',strtotime($posts[$key]['post_date']));
+            }
+        }
+
+        $this->assign('pages',$pages);
+        $this->assign('pn',$Page->getPn());
+
         $this->assign('posts',$posts);
     	$this->display("Home:$tplname");
 	}
